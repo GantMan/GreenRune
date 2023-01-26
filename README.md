3a035a66f3a0c1caac04ed29e400b5b47ee9cd0f
Gant Laborde <gantman@gmail.com>
2023-01-26 11:13:29 -0800

    getting region info

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 1. ~~Make JSON corresponding to each alphabet image (greenGlyphs.js)~~
 1. ~~Encode normal strings into vowel images (greenPrep function)~~
 1. Create metadata site
-    1. Load all symbols into list
-    1. Select a symbol and get barycentric coordinates of drawable regions
+    1. ~~Load all symbols into list~~
+    1. ~~Select a symbol and get barycentric coordinates of drawable regions~~
     1. Use this to re-update the JSON for coordinates of sub glyphs
 1. Create ideal glyph algorithm to identify how many regions are ideal for a glyph in a word
--- src/App.js
+++ src/App.js
@@ -2,6 +2,74 @@ import "./App.css";
 import { useState } from "react";
 import { greenGlyphs } from "./greenGlyphs";
 
+let regions = []
+
+function initDraw(canvas) {
+  const ctx = canvas.getContext("2d");
+  regions = []
+  var mouse = {
+    x: 0,
+    y: 0,
+    startX: 0,
+    startY: 0,
+  };
+  function setMousePosition(e) {
+    const rect = canvas.getBoundingClientRect();
+    let ev = e || window.event; //Moz || IE
+    const resizeRatio = canvas.width/ canvas.clientWidth;
+    const { pageX, pageY } = ev.touches ? ev.touches[0] : ev;
+    mouse.x = (pageX - rect.left - window.pageXOffset) * resizeRatio;
+    mouse.y = (pageY - rect.top - window.pageYOffset) * resizeRatio;
+  }
+
+  let element = null;
+  canvas.onmousemove = function (e) {
+    setMousePosition(e);
+
+  };
+
+  function log(mouse) {
+    const canWidth = 1947;
+    const canHeight = 1609;
+    const { x, y, startX, startY } = mouse;
+
+    // Sort the x and y values to make sure the start is before the end and make proportional rounded to 2 decimal places
+    const resizedXs = [startX, x].map((x) => (x / canWidth).toFixed(2)).sort();
+    const resizedYs = [startY, y].map((y) => (y / canHeight).toFixed(2)).sort();
+
+    regions.push([resizedXs[0], resizedYs[0], resizedXs[1], resizedYs[1]])
+    document.getElementById("regions").value = JSON.stringify(regions)
+  }
+
+  canvas.onclick = function (e) {
+    const { x, y, startX, startY } = mouse;
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 10, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'green';
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#003300';
+    ctx.fill();
+    ctx.stroke();
+
+    if (element !== null) {
+      const { x, y, startX, startY } = mouse;
+      log(mouse)
+      ctx.beginPath();
+      ctx.rect(startX, startY, x - startX, y - startY);
+      ctx.stroke();
+      element = null;
+      canvas.style.cursor = "default";
+      console.log("done.", mouse);
+    } else {
+      console.log("begun.");
+      mouse.startX = mouse.x;
+      mouse.startY = mouse.y;
+      element = true;
+      canvas.style.cursor = "crosshair";
+    }
+  };
+}
+
 function App() {
   const [selectedOption, setSelectedOption] = useState();
   const topGlyphs = Object.keys(greenGlyphs);
@@ -20,6 +88,8 @@ function App() {
       context.drawImage(loadGlyph, 0, 0, canvas.width, canvas.height);
     };
     loadGlyph.src = `/letters/${gl}`;
+    // allows draw to canvas (JS) blech
+    initDraw(canvas);
   }
 
   function GlyphDropdown() {
@@ -38,6 +108,7 @@ function App() {
           ))}
         </select>
         <p>Showing: {selectedOption}</p>
+        <textarea id="regions" />
       </>
     );
   }
